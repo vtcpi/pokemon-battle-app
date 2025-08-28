6a8e760b352f2a11cba27c4b135735299aa95d4b
vtcpi <78043894+vtcpi@users.noreply.github.com>
2025-08-27 22:31:00 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2GieCVOqe0tv-JcvS-xG4C9hi252tBP0",
+  authDomain: "pokemon-screen-time-battle.firebaseapp.com",
+  projectId: "pokemon-screen-time-battle",
+  storageBucket: "pokemon-screen-time-battle.firebasestorage.app",
+  messagingSenderId: "912492141101",
+  appId: "1:912492141101:web:c7c784fca7a688542c76a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
